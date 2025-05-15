6387b447be783bcda98bbca78c617e8a7851f4ad
Matheus Maciel <111710046+MatheusMaciel00@users.noreply.github.com>
2025-05-15 15:27:29 -0300

    Add files via upload

---
+++ Praticas de python/projetonotas.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# incializar listas
+resultado_corrida = []
+resultado_flexao = []
+resultado_abdominal = []
+
+num_alunos = 10 
+
+print("Digite os resultados dos alunos:\n")
+
+for i range(num_alunos):
+    print(f"\naluno {i+1}:")
